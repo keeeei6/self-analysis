f049e35884c7143fd218193558bb2db43096f5d6
keeeei6 <65399740+keeeei6@users.noreply.github.com>
2020-07-10 00:28:33 +0900

    question,answer表示作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,22 +1,21 @@
 class QuestionsController < ApplicationController
 
   def index
-    @questions = Question.all
-      # @answer = Answer.find_by(question_id: params[:id],user_id: current_user.id)
-      # if @answer
-      #   render("questions/show")
-      # else
-      #   render("answers/new")
-      # end
+    @questions = Question.where(group_id: params[:group_id])
   end
 
   def show
     @answer = Answer.find_by(question_id: params[:id],user_id: current_user.id)
+    if @answer.present?
+      render 'show'
+    else
+      redirect_to group_questions_path(group_id: params[:group_id])
+    end
   end
 
-  private
-  def answer_params
-    params.require(:answer).permit(:answer, :question_id)
-  end
+  # private
+  # def answer_params
+  #   params.require(:answer).permit(:answer, :question_id)
+  # end
  
 end
