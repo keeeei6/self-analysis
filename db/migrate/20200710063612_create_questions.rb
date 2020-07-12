565aea59786fc45a7db9bee9c0137afbe38298e0
keeeei6 <65399740+keeeei6@users.noreply.github.com>
2020-07-12 19:27:02 +0900

    修正

--- db/migrate/20200710063612_create_questions.rb
+++ db/migrate/20200710063612_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[5.0]
   def change
     create_table :questions do |t|
       t.string :content
-      t.references :group,  foreign_key: true
+      t.references :group, foreign_key: true
       t.timestamps
     end
   end
